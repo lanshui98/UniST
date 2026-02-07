19d88ea936107e357db880e8e50373ca3d2ff922
Lan Shui <lshui@mdanderson.org>
2026-02-06 18:49:49 -0600

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -11,10 +11,11 @@ __version__ = "0.1.0"
 __author__ = "Lan Shui"
 __email__ = "lshui@mdanderson.org"
 
-# Users should import directly from submodules:
-#   from metrics import dice_coefficient, boundary_iou
-#   from downstream.morph import dilation, erosion
-#   from downstream.vis import slice_points_plane, slice_stack
-#   from interpolation.ReHo import interpolate_missing_slices_spline
+# Import submodules from unist package:
+#   import unist
+#   from unist.metrics import dice_coefficient, boundary_iou
+#   from unist.downstream.morph import dilation, erosion
+#   from unist.downstream.vis import slice_points_plane, slice_stack
+#   from unist.interpolation.ReHo import interpolate_missing_slices_spline
 
 __all__ = ["__version__", "__author__", "__email__"]
